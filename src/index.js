10631fde470c176cbfaeb35f309b0b412196cec9
Leandro Ostera <leandro@ostera.io>
2016-11-05 21:09:59 +0100

    Stuff

---
+++ src/index.js
@@ -0,0 +1,74 @@
+//@flow
+
+import {
+  tick as now,
+  log,
+  atom,
+} from 'scheduler/utils'
+
+type Time = number
+
+type Predicate = Function
+
+// Wrapper around a function
+type Task = {
+  run(): void;
+  defer(): Promise;
+}
+const task = (fn: Predicate): Task => ({
+  run: () => {
+    try {
+      return fn()
+    } catch (e) {
+      throw e;
+    }
+  },
+
+  defer: () => Promise.resolve().then(this.run)
+})
+
+// Time-Ordered Priority Queue
+type Timeline = {
+  add(time: Time, task: Task): void;
+  get(from: Time, to: Time): Task[];
+}
+const timeline = (): Timeline => {
+  const tasks = []
+
+  const add = (t, x) => {
+    tasks.push({time: t, task: x})
+  }
+
+  const get = (a, b) => [tasks.pop()]
+
+  return {
+    add: add,
+    get: get
+  }
+}
+
+// Self-referencing Timeline consumer
+type Scheduler = {
+  schedule(when: Time, task: Task): Task;
+  run(): void;
+}
+const scheduler = (): Scheduler => {
+  const __timeline = timeline()
+  let last_run = -1
+
+  const schedule = (w, t) => {
+    __timeline.add(w, t)
+  }
+
+  const run = () => {
+    // Get all the tasks in the timeline between
+    // the last run and right now
+    __timeline
+      .get(last_run, now())
+      .map( x => x.defer() )
+  }
+
+  return { schedule, run }
+}
+
+
