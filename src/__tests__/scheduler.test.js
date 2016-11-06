6ffcaac344d35ee6b1584d15f883f1b1a63ab455
Leandro Ostera <leandro@ostera.io>
2016-11-06 01:36:40 +0100

    multiple scheduled tasks

--- src/__tests__/scheduler.test.js
+++ src/__tests__/scheduler.test.js
@@ -9,11 +9,14 @@ import {
 } from 'scheduler'
 
 test('adds tasks', async () => {
-  let t = task( () => true )
+  let t = (x) => task( () => log("What! I'm ALIVE", x) )
   let s = scheduler()
-  s.schedule(now(), t)
-  s.schedule(now(), t)
-  s.schedule(now(), t)
-  s.schedule(now(), t)
-  s.run()
+  s.schedule(now(), t(1))
+  s.schedule(now(), t(2).delay(1000))
+  s.schedule(now(), t(3).delay(1000))
+  s.schedule(now(), t(4).delay(1000))
+  s.schedule(now(), t(5).delay(1000))
+  s.schedule(now(), t(6).delay(1000))
+  s.schedule(now(), t(7).delay(1000))
+  s.schedule(now(), t(8).delay(1000))
 })
