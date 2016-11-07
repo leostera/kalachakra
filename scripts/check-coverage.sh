741dad190fd8cbf6990c465145af784ee868e87a
Leandro Ostera <leandro@ostera.io>
2016-11-07 22:25:03 +0100

    Don't type check coverage + tabular type check coverage report

--- scripts/check-coverage.sh
+++ scripts/check-coverage.sh
@@ -12,12 +12,14 @@ readonly FILE_COUNT=$(echo "${FILES}" | sed 's/ /\n/g' | wc -l)
 
 echo ${FILE_COUNT}
 TOTAL_PERCENTAGE=0.0
+OUTPUT=""
 for file in ${FILES}; do
   COVERAGE=$(./node_modules/.bin/flow coverage ${file})
   PERCENTAGE=$(echo "${COVERAGE}" | awk -F': ' '{ print $2}' | awk -F'% ' '{ print $1}') 
   TOTAL_PERCENTAGE="$(echo "${PERCENTAGE} + ${TOTAL_PERCENTAGE}" | bc)"
-  echo "${file} - ${COVERAGE}"
+  OUTPUT="${OUTPUT}${file} - ${COVERAGE}\n"
 done
 
+echo -e $OUTPUT | column -t
 readonly TOTAL="$( echo "${TOTAL_PERCENTAGE}/${FILE_COUNT}.0" | bc)"
 echo "== Coverage: ${TOTAL}%"
